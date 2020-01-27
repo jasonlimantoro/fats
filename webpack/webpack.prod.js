81e7a2dbe57294c904a742e112109b948abb7f07
jasonlimantoro <jasonlimantoro99@gmail.com>
2020-01-28 00:35:00 +0800

    [FIX](Webpack): fix webpack prod config

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -42,6 +42,7 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           'css-loader',
+          'postcss-loader',
           {
             loader: 'sass-loader',
             options: {
