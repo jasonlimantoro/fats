3d7d4429200f1f152ca618d2c9b18958684fb4fd
jasonlimantoro <jasonlimantoro99@gmail.com>
2020-01-30 14:02:13 +0800

    [CHORE]: remove route to home

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { routes } from 'config/routes';
-import Home from './Home';
 import Login from './Login';
 import Panel from './Panel';
 import GuestRoute from './GuestRoute';
 
 export default () => (
   <Switch>
-    <Route exact path={routes.home} component={Home} />
     <GuestRoute path={routes.login} component={Login} />
     <Route path={String(routes.panel)} component={Panel} />
-    <Redirect to={routes.home} />
+    <Redirect to={routes.login} />
   </Switch>
 );
