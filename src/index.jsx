30e5ae47ad74262cc858216e9ad0742ce420b366
jasonlimantoro <jasonlimantoro99@gmail.com>
2020-02-01 03:22:32 +0800

    [CHORE]: remove mirage for development

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { startMirage } from 'lib/mirage';
 import Root from './components/Root';
 import './stylesheets/app.scss';
-
-if (process.env.NODE_ENV === 'development') {
-  startMirage();
-}
 
 ReactDOM.render(<Root />, document.getElementById('app'));
