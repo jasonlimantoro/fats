80d9b172a8b46563b932124fed3427a9ec07c843
jasonlimantoro <jasonlimantoro99@gmail.com>
2020-02-04 22:27:52 +0800

    [FIX](SessionDetail): fix logic for finding the registered students

--- src/modules/ui/sessionDetail/selector.js
+++ src/modules/ui/sessionDetail/selector.js
@@ -26,32 +26,27 @@ export const selectStudentList = createSelector(
   (state, sessionId) => {
     if (isEmpty(state.data.schedules)) return [];
     const schedule = state.data.schedules[sessionId];
-    const allStudents = state.data.students;
+    const registeredStudents = state.data.labs[schedule.lab].students;
     const { attendances } = state.data;
     const LATE_LIMIT = 15;
-    return Object.values(allStudents).reduce((accum, current) => {
-      if (current.attendanceId && attendances[current.attendanceId]) {
-        const attendance = attendances[current.attendanceId];
+    return registeredStudents.map(current => {
+      const student = state.data.students[current];
+      if (student.attendanceId && attendances[student.attendanceId]) {
+        const attendance = attendances[student.attendanceId];
         const isLate = moment(attendance.created_at).diff(moment(schedule.time), 'minutes') > LATE_LIMIT;
-        return [
-          ...accum,
-          {
-            ...current,
-            matric: current.user_id,
-            status: isLate ? 'late' : 'present',
-            time: moment(attendance.created_at).format('HH:mm'),
-          },
-        ];
+        return {
+          ...student,
+          matric: student.user_id,
+          status: isLate ? 'late' : 'present',
+          time: moment(attendance.created_at).format('HH:mm'),
+        };
       }
-      return [
-        ...accum,
-        {
-          ...current,
-          matric: current.user_id,
-          status: 'absent',
-          time: '-',
-        },
-      ];
+      return {
+        ...student,
+        matric: student.user_id,
+        status: 'absent',
+        time: '-',
+      };
     }, []);
   },
 );
