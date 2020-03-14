6277a4ddfcb8ea45671aff49e7f81c5984446576
jasonlimantoro <jasonlimantoro99@gmail.com>
2020-03-14 21:50:50 +0800

    [FIX](sessionsReducer): add the new session at the begining of the array

--- src/modules/ui/sessions/reducer.js
+++ src/modules/ui/sessions/reducer.js
@@ -10,7 +10,7 @@ export default function reducer(state = initialState, action) {
     case actionTypes.FEED_DATA:
       return state.set('sessionIds', fromJS(action.payload.sessionIds || []));
     case actionTypes.ADD_DATA:
-      return state.update(`${action.entity}Ids`, ids => ids.push(action.payload));
+      return state.update(`${action.entity}Ids`, ids => ids.unshift(action.payload));
     case actionTypes.REMOVE_DATA:
       return state.update(`${action.entity}Ids`, ids => ids.filter(id => id !== action.payload));
     default:
